9af04f0801e1499a39d745cab44a864ea96b393a
ejens0804 <ejens0804@byui.edu>
2025-02-01 16:10:35 -0700

    finished updating the javascript for coolpics and the css for coolpics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,46 @@
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+    menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+handleResize();
+window.addEventListener("resize", handleResize);
+
+
+function viewerTemplate(pic, alt) {
+    return ` <div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+    const clickedImage = event.target;
+	// get the src attribute from that element and 'split' it on the "-"
+    const parts = clickedImage.src.split("-");
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const newSrc = `${parts[0]}-full.jpeg`;
+	// insert the viewerTemplate into the top of the body element
+	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSrc, clickedImage.alt));
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+let gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", viewHandler);
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
